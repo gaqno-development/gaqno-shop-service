c74be20b319ef1bfc30ca98c683580fa754d40ef
Gabriel Aquino <gabriel.aquino@outlook.com>
2026-04-27 17:47:23 -0300

    chore: adopt backcore jest-preset in all services

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,15 @@
+const base = require('@gaqno-development/backcore/jest-preset');
+
 /** @type {import('jest').Config} */
 module.exports = {
-  moduleFileExtensions: ["js", "json", "ts"],
-  rootDir: ".",
-  testRegex: ".*\\.spec\\.ts$",
-  transform: {
-    "^.+\\.(t|j)s$": [
-      "ts-jest",
-      {
-        diagnostics: { ignoreCodes: [151002] },
-      },
-    ],
-  },
-  collectCoverageFrom: ["src/**/*.(t|j)s"],
+  ...base,
+  collectCoverageFrom: ['src/**/*.(t|j)s'],
   coveragePathIgnorePatterns: [
-    "/node_modules/",
-    "/dist/",
-    "src/main.ts",
-    "src/database/migrate.ts",
-    "src/database/migrations/",
+    '/node_modules/',
+    '/dist/',
+    'src/main.ts',
+    'src/database/migrate.ts',
+    'src/database/migrations/',
   ],
-  coverageDirectory: "./coverage",
-  testEnvironment: "node",
-  moduleNameMapper: {
-    "^src/(.*)$": "<rootDir>/src/$1",
-  },
-  setupFiles: ["<rootDir>/test/jest.setup.ts"],
+  setupFiles: ['<rootDir>/test/jest.setup.ts'],
 };
